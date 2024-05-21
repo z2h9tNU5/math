c44223ca9ac056a231e3ca80435a5c72e294a3d1
z2h9tNU5 <aumeda@kind.ocn.ne.jp>
2024-05-21 17:07:44 +0900

    add ChineseRemainderTheorem

--- math.go
+++ math.go
@@ -1,6 +1,12 @@
 // Package math implement something that is often used in mathematical logic.
 package math
 
+import "fmt"
+
+var (
+	ErrChineseRemainderTheorem = fmt.Errorf("first and second arguments must be disjoint")
+)
+
 // Gcd returns the greatest common divisor.
 func Gcd(a, b int) int {
 	if b == 0 {
@@ -17,4 +23,22 @@ func MultipleGcd(val []int) int {
 	}
 
 	return z
+}
+
+// ChineseRemainderTheorem Chinese remainder theorem.
+func ChineseRemainderTheorem(m1, m2, b1, b2 int) (int, error) {
+	if Gcd(m1, m2) != 1 {
+		return 0, ErrChineseRemainderTheorem
+	}
+
+	var ret int
+	for i := 0; i < m1*m2; i++ {
+		z := m1*i + b1
+		if z%m2 == b2 {
+			ret = z
+			break
+		}
+	}
+
+	return ret, nil
 }
